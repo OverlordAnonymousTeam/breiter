b22de80f42207d024677ae68e3c6e66a5cfc1597
OverlordAnonymousTeam <au_au_2015@bk.ru>
2022-09-21 17:53:51 +0300

    first commit

---
+++ offline.js
@@ -0,0 +1,20 @@
+﻿{
+	"version": 1663771962,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/брейтер-sheet0.png",
+		"images/Спрайт-sheet0.png",
+		"images/Спрайт2-sheet0.png",
+		"images/Спрайт3-sheet0.png",
+		"images/Спрайт4-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
